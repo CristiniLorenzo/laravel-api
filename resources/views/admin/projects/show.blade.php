8ce6b079864258af7e48ec340406435bd84a8729
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-06-12 16:35:57 +0200

    first commit

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>{{ $project->name }}</h1>
+
+    <div>
+        <strong>ID:</strong> {{ $project->id }}
+    </div>
+
+    <div>
+        <strong>Slug:</strong> {{ $project->slug }}
+    </div>
+
+    <div>
+        <strong>Type:</strong> {{ $project->type ? $project->type->name : 'No type'}}
+    </div>
+
+    <div>
+        <strong>Technologies</strong>:
+        @if (count($project->technologies) > 0)
+            @foreach ($project->technologies as $technology)
+                {{ $technology->name }}@if (!$loop->last),@endif
+            @endforeach
+        @else
+            NONE
+        @endif
+    </div>
+
+    <div>
+        <strong>Created at:</strong> {{ $project->created_at }}
+    </div>
+
+    <div>
+        <strong>Updated at:</strong> {{ $project->updated_at }}
+    </div>
+
+    @if ($project->summary)
+        <div class="mt-3">
+            <strong>Summary:</strong> {{ $project->summary }}
+        </div>       
+    @endif
+@endsection
